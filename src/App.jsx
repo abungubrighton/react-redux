a7b0e37ac88f7526da234f353feaf921944cccac
abungubrighton <brighton@ibuqacapital.com>
2023-10-16 04:14:33 +0300

    chore: cleanup body style  and add postform component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import './App.css';
+import PostForm from './components/PostForm';
 import Posts from './components/Posts';
 
 function App() {
     return ( 
         <div className = "App" >
+            <PostForm />
+            <hr />
             <Posts/>
         </div>
     );
