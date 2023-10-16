118f0e4e99c7944a220eb85164a915215d497b66
abungubrighton <brighton@ibuqacapital.com>
2023-10-16 04:37:16 +0300

    chore: set input values as part of component state, with their handlers

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,8 +4,18 @@ class PostForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            title:'',
+            body:'',
             
         };
+        // bind the class(this) to onchange attribute
+        this.onChange = this.onChange.bind(this);
+    }
+    // event handler called onChange , 
+    //the one called in this manner {this.onChange}
+    // could be any name
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value});
     }
   render() {
     return (
@@ -15,13 +25,13 @@ class PostForm extends Component {
             <div>
                 <label>Title</label>
                 <br />
-                <input type="text" name='title'/>
+                <input type="text" name='title' value={this.state.title} onChange={this.onChange}/>
             </div>
             <br />
             <div>
                 <label>Body:</label>
                 <br />
-                <input type="text" name='body'/>
+                <input type="text" name='body' value={this.state.body} onChange={this.onChange}/>
             </div>
             <button type='submit'>Submit</button>
         </form>
